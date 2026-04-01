089f5fd457758b93ed4f4c36c2c84817b55807d5
Gumovski <Gumovskijstepan@gmail.com>
2026-04-01 16:16:14 +0300

    Обновлен статус задания № 201 Тема: 2 > Неверно

--- Тема 2/Задания/-Задание 201.py
+++ Тема 2/Задания/-Задание 201.py
@@ -1,12 +1,20 @@
 # Решение
+print('x y w z')
+for x in range(2):
+    for y in range(2):
+        for w in range(2):
+            for z in range(2):
+                F1 = (x or (not y)) <= (w == z)
+                F2 = (x or (not y)) == (w <= z)
+                if F1 == 0:
+                    print(x,y,w,z)
 
 
 
 
 
 
-
-answer = 'xyzw'
+answer = '.w..'
 
 #
 
