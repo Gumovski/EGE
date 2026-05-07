c65bb5dd933f978c2f6a3531dd23586a946a960b
Gumovski <Gumovskijstepan@gmail.com>
2026-05-07 14:35:46 +0300

    Обновлен статус задания № 15 Тема: 6 > Неверно

---
+++ Тема 6/Задания/-Задание 15.py
@@ -0,0 +1,30 @@
+# Решение
+from turtle import *
+tracer(0)
+screensize(2000,2000)
+koef = 40
+
+for i in range(4):
+    forward(14 * koef)
+    right(90)
+for i in range(5):
+    forward(5 * koef)
+    right(45)
+
+up()
+
+for x in range(-20,20):
+    for y in range(-20,20):
+        goto(x*koef, y * koef)
+        dot(2)
+
+exitonclick()
+
+
+answer = 63
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(6, 15, answer, '093f65e080a295f8076b1c5722a46aa2'))
