99948ea8506ef1d13dd7ec9001b628f2d25c93fc
Gumovski <Gumovskijstepan@gmail.com>
2026-05-07 14:38:14 +0300

    Решение Задач

--- Тема 5/Задания/Задание 51.py
+++ Тема 5/Задания/Задание 51.py
@@ -1,4 +1,20 @@
 # Решение
+c = 0
+for n in range(13,100000):
+    s = bin(n)[2:]
+    n1 = n % 3
+    n2 = bin(n1)[2:]
+    s +=  '0' + n2
+    t = int(s,2)
+    t1 = t % 5
+    t2 = bin(t1)[2:]
+    s += '0' + t2
+
+    r = int(s,2)
+    f = int(s)
+    if f > 1111111110  and f < 1444444416:
+        c += 1
+print(c)
 
 
 
