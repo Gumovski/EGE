55afb1d4e966b068a88b42b9e039179943cddfad
Gumovski <Gumovskijstepan@gmail.com>
2026-09-02 17:42:12 +0300

    Решение Задач

--- Тема 9/Задания/Задание 18.py
+++ Тема 9/Задания/Задание 18.py
@@ -1,5 +1,30 @@
 # Решение
+count_rows = 0
+with open('Задание 18.txt') as file:
+    mat = []
+    for line in file:
+        s = list(map(int,line.split('\t')))
+        mat.append(s)
+    st = []
+    for i in range(len(mat[0])):
+        ss = []
+        for row in mat:
+            ss.append(row[i])
+        st.append(ss)
+    for i in range(len(st)):
+        for j in range(len(st[0])):
+            c = 0
+            t = st[i][j]
+            for k in range(len(st[0])):
+                if st[0][k] == t:
+                    c += 1
+                if 170 <= c:
+                    continue
+                else:
+                    mat[j][i] = '+'
 
+for row in mat:
+    print(mat)
 
 
 
