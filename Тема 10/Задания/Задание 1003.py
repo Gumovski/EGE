3c9d9035c2ee1c31a5d177cb9d0c300f6d18d0ec
Андрей Боев <a.boev.it@gmail.com>
2026-09-10 16:05:28 +0300

    NEW: Тема 10

---
+++ Тема 10/Задания/Задание 1003.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(10, 1003, answer, '35f4a8d465e6e1edc05f3d8ab658c551'))
---
+++ Тема 10/Задания/Задание 1008.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(10, 1008, answer, 'cfcd208495d565ef66e7dff9f98764da'))
