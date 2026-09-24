800ef0102e338e90adcf6d40445e155c65ab6e2f
Андрей Боев <a.boev.it@gmail.com>
2026-09-24 16:09:28 +0300

    Тема 13

---
+++ Тема 13/Задания/Задание 1307.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(13, 1307, answer, '26657d5ff9020d2abefe558796b99584'))
